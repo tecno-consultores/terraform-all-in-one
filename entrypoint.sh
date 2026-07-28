9515a8dd4221b3fb3c7377d63a372995d8a93774
Sinfallas <sinfallas@yahoo.com>
2026-07-28 09:30:43 -0400

    Condense semaphore user add command in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-semaphore user add --admin \
-  --login admin \
-  --password 0n0qNwFTHSMdd6i2m0xxukAuuVluppKD \
-  --name "admin" \
-  --email "admin@localhost" \
-  --config /etc/semaphore/config.json || true
-
+semaphore user add --admin --login admin --password 0n0qNwFTHSMdd6i2m0xxukAuuVluppKD --name "admin" --email "admin@localhost" --config /etc/semaphore/config.json || true
 exec semaphore server --config /etc/semaphore/config.json
